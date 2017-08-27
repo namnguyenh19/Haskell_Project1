cd36b32476f94269e29ed5c9f431602d76398676
Nam Nguyen <namn1@student.unimelb.edu.au>
2017-08-27 14:01:23 +1000

    gamestate update

--- Proj1.hs
+++ Proj1.hs
@@ -9,35 +9,31 @@ import Data.Char
 data Note = A | B | C | D | E | F | G
     deriving (Eq, Show)
 
-data Octave = 1 | 2 | 3
-    deriving (Eq, Show)
-
-type Pitch = (Note,Octave)
-    deriving (Eq, Show)
+type Pitch = (Note, Int)
 
 type Chord = [Pitch]
-    deriving (Eq, Show)
 
 type Answer = [Int]
-    deriving (Eq, Ord, Show)
 
 -- GameState stores a list of target Pitch correctly guessed & previous guess
-data GameState = ([Pitch], Chord)
+type GameState = ([Pitch], [Chord], [Answer])
 
 -- helper functions
 
 getNote :: String -> Note
-getNote x = x!!0
+getNote = head
 
 getOctave :: String -> Octave
 getOctave x = x!!1
+
+remainNotes :: 
 
 -- main functions
 
 -- always guess A1, B2, C3
 initialGuess :: ([String], GameState)
-initialGuess = (["A1","B2","C3"], ([],[]))
+initialGuess = (["A1","B2","C3"], [(A,1), (B,2), (C,3)], [] )
 
 nextGuess :: ([String], GameState) -> (Int, Int, Int) -> ([String], GameState)
-nextGuess ([g1, g2, g3], (targets, lastguess)) (cPitch, cNote, cOctave) =
-    
+nextGuess ([g1, g2, g3], (foundTargets, guesses, results) (cPitch, cNote, cOctave)
+  | cPitch == 0 && cNote == 0 && cOctave == 0
